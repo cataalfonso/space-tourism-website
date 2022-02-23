ebf4a659b837dfe3074b7583d4b8f57df81bc4a3
Cata Alfonso <cataalfonso@Catas-MacBook-Air.local>
2022-02-24 00:32:38 +0100

    animating nav with js

---
+++ design-system/index.js
@@ -0,0 +1,14 @@
+const nav = document.querySelector('.primary-navigation');
+const navToggle= document.querySelector('.mobile-nav-toggle');
+
+// when someone clicks on the hamburguer button
+navToggle.addEventListener('click', ()=>{
+    const visibility= nav.getAttribute("data-visible");
+    //if the nav is closed, open it
+    if (visibility === "false"){
+        nav.setAttribute("data-visible", true);
+    } else{
+ // if the nav is open, close it
+        nav.setAttribute("data-visible", false); 
+    }
+})
