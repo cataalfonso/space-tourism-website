ec5e2d3454f4b33ec3418f71fa56cd1446745c8e
Cata Alfonso <cataalfonso@Catas-MacBook-Air.local>
2022-03-04 01:25:02 +0100

    setting dinamic content on destination page-images

--- design-system/tabs.js
+++ design-system/tabs.js
@@ -1,56 +1,61 @@
-const tabList= document.querySelector('[role="tablist"]');
-const tabs= tabList.querySelectorAll('[role="tab"]');
+const tabList = document.querySelector('[role="tablist"]');
+const tabs = tabList.querySelectorAll('[role="tab"]');
 
 tabList.addEventListener('keydown', changeTabFocus)
 
-tabs.forEach((tab)=>{
+tabs.forEach((tab) => {
     tab.addEventListener('click', changeTabPanel)
-} )
+})
 
-let tabFocus=0
-function changeTabFocus(e){
-    const keydownLeft=37;
-    const keydownRight=39;
+let tabFocus = 0
+function changeTabFocus(e) {
+    const keydownLeft = 37;
+    const keydownRight = 39;
     //change the tabIndex of the current tab to -1
-    if(e.keyCode=== keydownLeft || e.keyCode===keydownRight){
+    if (e.keyCode === keydownLeft || e.keyCode === keydownRight) {
         tabs[tabFocus].setAttribute("tabindex", -1);
-    }
-    //if the right key is pushed, move to the next tab on the right
-    if (e.keyCode===keydownRight){
-        tabFocus++;
-        if (tabFocus>=tabs.length){
-            tabFocus=0;
+
+        //if the right key is pushed, move to the next tab on the right
+        if (e.keyCode === keydownRight) {
+            tabFocus++;
+            if (tabFocus >= tabs.length) {
+                tabFocus = 0;
+            }
+        } else if (e.keyCode === keydownLeft) {
+        //if the left key is pushed, move to the next tab on the left
+            tabFocus--;
+            if (tabFocus < 0) {
+                tabFocus = tabs.length - 1;
+            }
         }
+        tabs[tabFocus].setAttribute("tabindex", 0);
+        tabs[tabFocus].focus();
     }
-    //if the left key is pushed, move to the next tab on the left
-    if (e.keyCode===keydownLeft){
-        tabFocus--;
-        if (tabFocus<0){
-            tabFocus=tabs.length-1;
-        }
-    }
-    tabs[tabFocus].setAttribute("tabindex",0);
-    tabs[tabFocus].focus();
+
 }
 
-function changeTabPanel(e){
-    const targetTab=e.target;
-    const targetPanel=targetTab.getAttribute("aria-controls");
-    const tabContainer=targetTab.parentNode;
-    const mainContainer=tabContainer.parentNode;
+function changeTabPanel(e) {
+    const targetTab = e.target;
+    const targetPanel = targetTab.getAttribute("aria-controls");
+    const targetImage = targetTab.getAttribute("data-image");
 
-    tabs.forEach(tab=>tab.setAttribute("aria-selected",false));
+    const tabContainer = targetTab.parentNode;
+    const mainContainer = tabContainer.parentNode;
+
+    tabContainer
+        .querySelector('[aria-selected="true"]')
+        .setAttribute("aria-selected", false);
     targetTab.setAttribute("aria-selected", true);
 
     mainContainer
-    .querySelectorAll('[role="tabpanel"]')
-    .forEach((panel)=>panel.setAttribute("hidden", true));
+        .querySelectorAll('[role="tabpanel"]')
+        .forEach((panel) => panel.setAttribute("hidden", true));
     mainContainer.querySelector([`#${targetPanel}`]).removeAttribute('hidden');
 
+    //hidden attribute is not working on pictures, so I had to use display, thus, I can't refactor this code
     mainContainer
-    .querySelectorAll('picture')
-    .forEach((picture)=>picture.setAttribute("hidden", true));
-
-    console.log(targetPanel);
+        .querySelectorAll('picture')
+        .forEach((picture) => picture.setAttribute("style", "display:none;"));
+    mainContainer.querySelector([`#${targetImage}`]).setAttribute("style", "display:block;");
 
 }
